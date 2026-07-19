d2d2a0a9b0af42ae2c073eafeae238e354b600c3
yavchris07 <wilondjayav27@gmail.com>
2026-07-19 16:27:30 +0200

    initialization

---
+++ src/components/router.tsx
@@ -0,0 +1,9 @@
+const Router = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Router
---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react'; //'@vitejs/react-typescript' // ou @vitejs/plugin-react selon votre template
+import tailwindcss from '@tailwindcss/vite';
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
+})
